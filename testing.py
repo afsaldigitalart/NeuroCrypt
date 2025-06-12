450842d148f87605f64408ec0b2dd8610dd4d678
Afsal <afsalshajibismi@gmail.com>
2025-06-12 21:41:31 +0530

    added README.MD and minor bug fixes

--- testing.py
+++ testing.py
@@ -3,7 +3,6 @@ import main as m
 
 SIZE = 64
 device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
-tests = ["help"]
 encrypter = m.Encrypter(SIZE)
 decrypter = m.Decrypter(SIZE)
 detective = m.Detective(SIZE)
@@ -16,35 +15,38 @@ encrypter.eval()
 decrypter.eval()
 detective.eval()
 
-for text in tests:
-    print(f"\nTesting: {text}")
-    chunks = m.text_to_bit(text, SIZE)
-    decrypter_outs, detective_outs = [], []
-    key = torch.randint(0, 2, (1, SIZE)).float().to(device)
-    nonce = torch.randint(0, 2, (1, SIZE)).float().to(device)
+text = input("Enter a Dummy Text: ")
 
-    for chunk in chunks:
-        msg = chunk.unsqueeze(0).to(device)
-        with torch.no_grad():
-            cipher = encrypter(msg, key, nonce)
-            decrypter_out = decrypter(cipher, key, nonce)
-            detective_out = detective(cipher)
+print(f"\nTesting: {text}")
+chunks = m.text_to_bit(text, SIZE)
+decrypter_outs, detective_outs = [], []
+key = torch.randint(0, 2, (1, SIZE)).float().to(device)
+nonce = torch.randint(0, 2, (1, SIZE)).float().to(device)
 
-        decrypter_outs.append(torch.sigmoid(decrypter_out).squeeze(0).cpu())
-        detective_outs.append(torch.sigmoid(detective_out).squeeze(0).cpu())
+for chunk in chunks:
+    msg = chunk.unsqueeze(0).to(device)
+    with torch.no_grad():
+        cipher = encrypter(msg, key, nonce)
+        decrypter_out = decrypter(cipher, key, nonce)
+        detective_out = detective(cipher)
 
-    decrypter_decoded = m.bit_to_text(decrypter_outs)
-    detective_decoded = m.bit_to_text(detective_outs)
+    decrypter_outs.append(torch.sigmoid(decrypter_out).squeeze(0).cpu())
+    detective_outs.append(torch.sigmoid(detective_out).squeeze(0).cpu())
 
-    print(f"Bob: {decrypter_decoded}")
-    print(f"Eve: {detective_decoded}")
-    if decrypter_decoded == text:
-        print("Bob: Perfect decryption!")
-    else:
-        print("Bob: Decryption failed.")
-    if detective_decoded == text:
-        print("Eve: SECURITY BREACH!")
-    elif "Error" in detective_decoded:
-        print("Eve: Failed (Good)")
-    else:
-        print("Eve: Partial decoding (Monitor)")
+decrypter_decoded = m.bit_to_text(decrypter_outs)
+detective_decoded = m.bit_to_text(detective_outs)
+
+print(f"Decrypter: {decrypter_decoded}")
+print(f"Detective: {detective_decoded}")
+
+if decrypter_decoded == text:
+    print("Decrypter: Perfect decryption!")
+else:
+    print("Decrypter: Decryption failed.")
+if detective_decoded == text:
+    print("Detective: SECURITY BREACH!")
+elif "Error" in detective_decoded:
+    print("Detective: Failed (Good Job!)")
+else:
+    print("Detective: Partial decoding")
+
